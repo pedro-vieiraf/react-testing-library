28409b6deabcd0ec8fbc5ac563151112853a59b1
Pedro Vieira <pedro258vf@gmail.com>
2023-06-08 17:25:56 -0300

    testando requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('Testes do App', () => {
+  it('A aplicação contém um conjunto fixo de links de navegação', () => {
+    renderWithRouter(<App />);
+    expect(screen.getAllByRole('link')[0]).toHaveTextContent('Home');
+    expect(screen.getAllByRole('link')[1]).toHaveTextContent('About');
+    expect(screen.getAllByRole('link')[2]).toHaveTextContent('Favorite Pokémon');
+  });
+});
+
+// acessar os elementos da tela
+
+// interagir com os elementos (se for necessário)
+
+// fazer os testes
